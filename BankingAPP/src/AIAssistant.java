373652ee66e699d14f1566dfe2b6abd5507a4519
PetruGlt <galateanupetru152@gmail.com>
2025-03-17 09:14:06 +0200

    initial commit

---
+++ BankingAPP/src/AIAssistant.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AIAssistant {
+    private List<Chat> chats;
+
+    public AIAssistant() {
+        this.chats = new ArrayList<>();
+    }
+
+    private void addChat(Chat chat) {
+        this.chats.add(chat);
+    }
+
+    public void handleAccountOpenRequests(Chat chat) {
+        chat.addMessage("Sure, I can help you open a new account. Could you please provide me with your personal details?");
+        this.addChat(chat);
+    }
+
+    public void provideCardInfo(Chat chat) {
+        chat.addMessage("Here is the information about our available cards: [Card Info]");
+        this.addChat(chat);
+    }
+
+    public void provideLoanInfo(Chat chat) {
+        chat.addMessage("Here is the information about our loan options: [Loan Info]");
+        this.addChat(chat);
+    }
+
+    public void guideCustomerThroughInitialApplicationForms(Chat chat) {
+        chat.addMessage("I will guide you through the initial application forms. Please fill in the following details: [Form Details]");
+        this.addChat(chat);
+    }
+
+    public void forwardComplexRequestsToHumanEmployee(Chat chat) {
+        chat.addMessage("Your request is a bit complex. I will forward it to one of our human employees. They will get back to you shortly.");
+        this.addChat(chat);
+    }
+
+    public void interactWithBankAPI(Chat chat) {
+        // Simulating an interaction with a bank API to retrieve updated information
+        String updatedInfo = "Retrieved updated information from the bank API.";
+        chat.addMessage(updatedInfo);
+        this.addChat(chat);
+    }
+
+    public String getChatHistory() {
+        StringBuilder history = new StringBuilder();
+        for (Chat chat : chats) {
+            history.append(chat.getMessages()).append("\n\n");
+        }
+        return history.toString();
+    }
+}
--- BankingAPP/src/Main.java
+++ BankingAPP/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BankAIAssistant assistant = new BankAIAssistant();
+        Chat chat1 = new Chat();
+        assistant.handleAccountOpenRequests(chat1);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Chat chat2 = new Chat();
+        assistant.provideCardInfo(chat2);
+
+        System.out.println(assistant.getChatHistory());
     }
 }
